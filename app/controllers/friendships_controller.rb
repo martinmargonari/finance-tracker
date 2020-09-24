b5c18024d607b7388afeeb89ff73de55e25ad5b4
Martin Margonari <martinmargonari@gmail.com>
2020-09-24 11:13:49 -0300

    Added My Friends page functionality

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,23 @@
+class FriendshipsController < ApplicationController
+
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "You are now following #{friend.first_name}"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+
+    redirect_to my_friends_path
+  end
+
+  def destroy
+    friend = User.find(params[:id])
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+
+    flash[:notice] = "Stopped following #{friend.first_name}"
+    redirect_to my_friends_path
+  end
+end
